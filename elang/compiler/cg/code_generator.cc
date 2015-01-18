5f5441e47877cb03f024b0f3a096955e4302d97d
Eval <eval1749@gmail.com>
2015-01-19 01:53:53 +0900

    elang/compiler/cg: WIP Implement |ast::Call| handling in |CodeGenerator|.

--- elang/compiler/cg/code_generator.cc
+++ elang/compiler/cg/code_generator.cc
@@ -7,14 +7,83 @@
 #include "base/logging.h"
 #include "elang/compiler/analyze/name_resolver.h"
 #include "elang/compiler/ast/class.h"
+#include "elang/compiler/ast/expressions.h"
 #include "elang/compiler/ast/method.h"
 #include "elang/compiler/ast/namespace.h"
 #include "elang/compiler/ast/statements.h"
 #include "elang/compiler/ast/visitor.h"
+#include "elang/compiler/cg/type_mapper.h"
 #include "elang/compiler/compilation_session.h"
+#include "elang/compiler/ir/nodes.h"
+#include "elang/compiler/predefined_names.h"
+#include "elang/hir/editor.h"
+#include "elang/hir/factory.h"
+#include "elang/hir/instructions.h"
+#include "elang/hir/types.h"
 
 namespace elang {
 namespace compiler {
+
+//////////////////////////////////////////////////////////////////////
+//
+// CodeGenerator::Output
+//
+struct CodeGenerator::Output {
+  hir::Instruction* instruction;
+  int position;
+  // When |type| is |nullptr|, no output is expected. It is different from
+  // |void| type. In this case, |position| must be -1.
+  hir::Type* type;
+
+  Output(hir::Type* type, hir::Instruction* instruction, int position)
+      : instruction(instruction), position(position), type(type) {
+    if (type) {
+      DCHECK_LE(position, 0);
+    } else {
+      DCHECK_EQ(position, -1);
+    }
+  }
+};
+
+//////////////////////////////////////////////////////////////////////
+//
+// CodeGenerator::ScopedOutput
+//
+class CodeGenerator::ScopedOutput {
+ public:
+  ScopedOutput(CodeGenerator* generator,
+               hir::Type* type,
+               hir::Instruction* instruction,
+               int position);
+  ScopedOutput(CodeGenerator* generator, hir::Instruction* instruction);
+  ~ScopedOutput();
+
+ private:
+  CodeGenerator* const generator_;
+  Output output_;
+  Output* const previous_output_;
+
+  DISALLOW_COPY_AND_ASSIGN(ScopedOutput);
+};
+
+CodeGenerator::ScopedOutput::ScopedOutput(CodeGenerator* generator,
+                                          hir::Type* type,
+                                          hir::Instruction* instruction,
+                                          int position)
+    : generator_(generator),
+      output_(type, instruction, position),
+      previous_output_(generator->output_) {
+  generator_->output_ = &output_;
+}
+
+CodeGenerator::ScopedOutput::ScopedOutput(CodeGenerator* generator,
+                                          hir::Instruction* instruction)
+    : ScopedOutput(generator, nullptr, instruction, -1) {
+}
+
+CodeGenerator::ScopedOutput::~ScopedOutput() {
+  generator_->output_ = previous_output_;
+}
 
 //////////////////////////////////////////////////////////////////////
 //
@@ -23,10 +92,13 @@ namespace compiler {
 CodeGenerator::CodeGenerator(CompilationSession* session,
                              hir::Factory* factory,
                              NameResolver* name_resolver)
-    : factory_(factory),
+    : editor_(nullptr),
+      factory_(factory),
       function_(nullptr),
       name_resolver_(name_resolver),
-      session_(session) {
+      output_(nullptr),
+      session_(session),
+      type_mapper_(new TypeMapper(factory, name_resolver)) {
 }
 
 CodeGenerator::~CodeGenerator() {
@@ -34,6 +106,14 @@ CodeGenerator::~CodeGenerator() {
 
 void CodeGenerator::Generate() {
   session_->global_namespace()->AcceptForMembers(this);
+}
+
+hir::Type* CodeGenerator::MapType(PredefinedName name) {
+  return type_mapper_->Map(name);
+}
+
+hir::Type* CodeGenerator::MapType(ir::Type* type) {
+  return type_mapper_->Map(type);
 }
 
 // ast::Visitor
@@ -44,7 +124,12 @@ void CodeGenerator::VisitAlias(ast::Alias* node) {
 }
 
 void CodeGenerator::VisitClass(ast::Class* clazz) {
-  clazz->AcceptForMembers(this);
+  DCHECK(clazz);
+  NOTREACHED();
+}
+
+void CodeGenerator::VisitClassBody(ast::ClassBody* node) {
+  node->AcceptForMembers(this);
 }
 
 void CodeGenerator::VisitEnum(ast::Enum* node) {
@@ -59,18 +144,31 @@ void CodeGenerator::VisitImport(ast::Import* node) {
   DCHECK(node);
 }
 
-void CodeGenerator::VisitMethod(ast::Method* method) {
+void CodeGenerator::VisitMethod(ast::Method* ast_method) {
+  DCHECK(!editor_);
   DCHECK(!function_);
-  // TODO(eval1749) NYI do following steps:
   //  1 Convert ast::FunctionType to hir::FunctionType
-  //  2 NewFunction()
-  method->body()->Accept(this);
-  methods_[method] = function_;
+  //  2 hir::NewFunction(function_type)
+  auto const method = name_resolver()->Resolve(ast_method)->as<ir::Method>();
+  if (!method) {
+    DVLOG(0) << "Not resolved " << *ast_method;
+    return;
+  }
+  function_ = factory()->NewFunction(
+      type_mapper()->Map(method->signature())->as<hir::FunctionType>());
+  hir::Editor editor(factory(), function_);
+  auto const return_instr = function_->entry_block()->last_instruction();
+  ScopedOutput scoped_output(this, return_instr->OperandAt(0)->type(),
+                             return_instr, 0);
+  ast_method->body()->Accept(this);
+  methods_[ast_method] = function_;
+  editor_ = nullptr;
   function_ = nullptr;
 }
 
 void CodeGenerator::VisitNamespace(ast::Namespace* node) {
   DCHECK(node);
+  NOTREACHED();
 }
 
 void CodeGenerator::VisitNamespaceBody(ast::NamespaceBody* node) {
@@ -92,6 +190,28 @@ void CodeGenerator::VisitBinaryOperation(ast::BinaryOperation* node) {
 
 void CodeGenerator::VisitCall(ast::Call* node) {
   DCHECK(node);
+  // TODO(eval1749) How do we support callee expression?
+  auto const callee = name_resolver()->ResolveCall(node);
+  DCHECK(callee) << "Unresolved call" << *node;
+  auto const callee_type =
+      MapType(callee->signature())->as<hir::FunctionType>();
+  // TODO(eval1749) We should make 'call' instruction takes multiple
+  // operands.
+  auto const call_instr = hir::CallInstruction::New(
+      factory(), output_->type, callee_type->GetDefaultValue(),
+      MapType(PredefinedName::Void)->GetDefaultValue());
+  editor_->InsertBefore(call_instr, output_->instruction);
+  auto position = static_cast<int>(node->arguments().size());
+  auto arguments = node->arguments().rbegin();
+  while (position) {
+    auto const arg_type = callee_type->parameters_type();
+    ScopedOutput output_to_argument(this, arg_type, call_instr, position);
+    (*arguments)->Accept(this);
+    --position;
+    ++arguments;
+  }
+  ScopedOutput output_callee(this, callee_type, call_instr, 0);
+  node->callee()->Accept(this);
 }
 
 void CodeGenerator::VisitConditional(ast::Conditional* node) {
@@ -116,6 +236,8 @@ void CodeGenerator::VisitMemberAccess(ast::MemberAccess* node) {
 
 void CodeGenerator::VisitNameReference(ast::NameReference* node) {
   DCHECK(node);
+  // TODO(eval1749) We need to have
+  // |NameResolver::GetReference(ast::Expression*)|.
 }
 
 void CodeGenerator::VisitUnaryOperation(ast::UnaryOperation* node) {
@@ -128,7 +250,15 @@ void CodeGenerator::VisitVariableReference(ast::VariableReference* node) {
 
 // Statement nodes
 void CodeGenerator::VisitBlockStatement(ast::BlockStatement* node) {
-  DCHECK(node);
+  for (auto const statement : node->statements()) {
+    if (statement == node->statements().back()) {
+      statement->Accept(this);
+    } else {
+      // Interleaved statement has no output.
+      ScopedOutput scoped_output(this, output_->instruction);
+      statement->Accept(this);
+    }
+  }
 }
 
 void CodeGenerator::VisitBreakStatement(ast::BreakStatement* node) {
@@ -148,7 +278,7 @@ void CodeGenerator::VisitEmptyStatement(ast::EmptyStatement* node) {
 }
 
 void CodeGenerator::VisitExpressionStatement(ast::ExpressionStatement* node) {
-  DCHECK(node);
+  node->expression()->Accept(this);
 }
 
 void CodeGenerator::VisitExpressionList(ast::ExpressionList* node) {
--- elang/compiler/cg/code_generator.h
+++ elang/compiler/cg/code_generator.h
@@ -12,13 +12,21 @@
 
 namespace elang {
 namespace hir {
+class Editor;
 class Factory;
 class Function;
+class Type;
 }
 namespace compiler {
+
+namespace ir {
+class Type;
+}
+
 class CompilationSession;
 class NameResolver;
 enum class TokenType;
+class TypeMapper;
 
 //////////////////////////////////////////////////////////////////////
 //
@@ -27,23 +35,37 @@ enum class TokenType;
 class CodeGenerator final : ast::Visitor {
  public:
   CodeGenerator(CompilationSession* session,
-               hir::Factory* factory,
-               NameResolver* name_resolver);
+                hir::Factory* factory,
+                NameResolver* name_resolver);
   ~CodeGenerator();
 
   void Generate();
   hir::Function* GetMethodFunction(ast::Method* method) const;
 
  private:
+  struct Output;
+  class ScopedOutput;
+
+  hir::Factory* factory() { return factory_; }
+  NameResolver* name_resolver() { return name_resolver_; }
+  TypeMapper* type_mapper() { return type_mapper_.get(); }
+
+  hir::Type* MapType(PredefinedName name);
+  hir::Type* MapType(ir::Type* type);
+
+// ast::Visitor
 #define V(Name) void Visit##Name(ast::Name* node) final;
   FOR_EACH_CONCRETE_AST_NODE(V)
 #undef V
 
+  hir::Editor* editor_;
   hir::Factory* const factory_;
   hir::Function* function_;
   std::unordered_map<ast::Method*, hir::Function*> methods_;
-  NameResolver* name_resolver_;
+  NameResolver* const name_resolver_;
+  Output* output_;
   CompilationSession* const session_;
+  const std::unique_ptr<TypeMapper> type_mapper_;
 
   DISALLOW_COPY_AND_ASSIGN(CodeGenerator);
 };
