2bd4dd650bbc161dbd26a516105539700e1e9b0a
Eval <eval1749@gmail.com>
2015-02-08 16:39:36 +0900

    elang/lir: Introduce |FactoryUser| for ease of using |Factory|.

---
+++ elang/lir/factory_user.cc
@@ -0,0 +1,68 @@
+// Copyright 2015 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "elang/lir/factory_user.h"
+
+#include "elang/lir/factory.h"
+#include "elang/lir/instructions.h"
+#include "elang/lir/value.h"
+
+namespace elang {
+namespace lir {
+
+//////////////////////////////////////////////////////////////////////
+//
+// FactoryUser
+//
+FactoryUser::FactoryUser(Factory* factory) : factory_(factory) {
+}
+
+FactoryUser::~FactoryUser() {
+}
+
+Value FactoryUser::NewRegister(Value type) {
+  return factory()->NewRegister(type);
+}
+
+// Creating instructions
+#define V(Name, ...)                                   \
+  Instruction* FactoryUser::New##Name##Instruction() { \
+    return factory()->New##Name##Instruction();        \
+  }
+FOR_EACH_LIR_INSTRUCTION_0_0(V)
+#undef V
+
+#define V(Name, ...)                                              \
+  Instruction* FactoryUser::New##Name##Instruction(Value input) { \
+    return factory()->New##Name##Instruction(input);              \
+  }
+FOR_EACH_LIR_INSTRUCTION_0_1(V)
+#undef V
+
+#define V(Name, ...)                                               \
+  Instruction* FactoryUser::New##Name##Instruction(Value input,    \
+                                                   Value input2) { \
+    return factory()->New##Name##Instruction(input, input2);       \
+  }
+FOR_EACH_LIR_INSTRUCTION_0_2(V)
+#undef V
+
+#define V(Name, ...)                                              \
+  Instruction* FactoryUser::New##Name##Instruction(Value output,  \
+                                                   Value input) { \
+    return factory()->New##Name##Instruction(output, input);      \
+  }
+FOR_EACH_LIR_INSTRUCTION_1_1(V)
+#undef V
+
+#define V(Name, ...)                                                         \
+  Instruction* FactoryUser::New##Name##Instruction(Value output, Value left, \
+                                                   Value right) {            \
+    return factory()->New##Name##Instruction(output, left, right);           \
+  }
+FOR_EACH_LIR_INSTRUCTION_1_2(V)
+#undef V
+
+}  // namespace lir
+}  // namespace elang
---
+++ elang/lir/factory_user.h
@@ -0,0 +1,67 @@
+// Copyright 2015 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ELANG_LIR_FACTORY_USER_H_
+#define ELANG_LIR_FACTORY_USER_H_
+
+#include "base/macros.h"
+#include "elang/lir/lir_export.h"
+#include "elang/lir/instructions_forward.h"
+#include "elang/lir/literals_forward.h"
+
+namespace elang {
+namespace lir {
+
+//////////////////////////////////////////////////////////////////////
+//
+// FactoryUser
+//
+class ELANG_LIR_EXPORT FactoryUser {
+ protected:
+  explicit FactoryUser(Factory* factory);
+  ~FactoryUser();
+
+  Factory* factory() const { return factory_; }
+
+  Value NewRegister(Value type);
+
+// New instructions
+#define V(Name, ...) Instruction* New##Name##Instruction();
+  FOR_EACH_LIR_INSTRUCTION_0_0(V)
+#undef V
+
+#define V(Name, ...) Instruction* New##Name##Instruction(Value input);
+  FOR_EACH_LIR_INSTRUCTION_0_1(V)
+#undef V
+
+#define V(Name, ...) \
+  Instruction* New##Name##Instruction(Value input, Value input2);
+  FOR_EACH_LIR_INSTRUCTION_0_2(V)
+#undef V
+
+#define V(Name, ...) \
+  Instruction* New##Name##Instruction(Value output, Value input);
+  FOR_EACH_LIR_INSTRUCTION_1_1(V)
+#undef V
+
+#define V(Name, ...) \
+  Instruction* New##Name##Instruction(Value output, Value left, Value right);
+  FOR_EACH_LIR_INSTRUCTION_1_2(V)
+#undef V
+
+#define V(Name, mnemonic, parameters, ...) \
+  Instruction* New##Name##Instruction parameters;
+  FOR_EACH_LIR_INSTRUCTION_N_N(V)
+#undef V
+
+ private:
+  Factory* const factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(FactoryUser);
+};
+
+}  // namespace lir
+}  // namespace elang
+
+#endif  // ELANG_LIR_FACTORY_USER_H_
