eval1749/elang

commit e147b2a514965260a6b84b1352d27b855527d33b

elang/compiler/syntax: Detect unbound variable reference in parser, e.g. |var x = x + 1|.

elang/compiler/syntax/parse_statement.cc
@@ -369,14 +369,13 @@ bool Parser::ParseForStatement(Token* for_keyword) {
         }
         auto const type = ConsumeType();
         auto const name = ConsumeToken();
+        auto const variable = factory()->NewVariable(for_keyword, type, name);
+        declaration_space_->AddMember(variable);
+        variables.push_back(variable);
         auto const init = AdvanceIf(TokenType::Assign) && ParseExpression()
                               ? ConsumeExpression()
                               : static_cast<ast::Expression*>(nullptr);
-
-        auto const variable =
-            factory()->NewVariable(for_keyword, type, name, init);
-        declaration_space_->AddMember(variable);
-        variables.push_back(variable);
+        variable->Bind(init);
 
         if (PeekToken() == TokenType::Colon) {
           if (init)
@@ -569,8 +568,7 @@ bool Parser::ParseTryStatement(Token* try_keyword) {
     LocalDeclarationSpace catch_var_scope(this, catch_keyword);
     if (PeekToken()->is_name()) {
       auto const catch_name = ConsumeToken();
-      catch_var = factory()->NewVariable(catch_keyword, catch_type, catch_name,
-                                         nullptr);
+      catch_var = factory()->NewVariable(catch_keyword, catch_type, catch_name);
       catch_var_scope.AddMember(catch_var);
     }
     if (!AdvanceIf(TokenType::RightParenthesis))
@@ -620,9 +618,10 @@ bool Parser::ParseUsingStatement(Token* using_keyword) {
 
     LocalDeclarationSpace using_scope(this, using_keyword);
     auto const var_type = NewTypeNameReference(var_keyword);
-    auto const variable = factory()->NewVariable(using_keyword, var_type,
-                                                 var_name, ConsumeExpression());
+    auto const variable =
+        factory()->NewVariable(using_keyword, var_type, var_name);
     using_scope.AddMember(variable);
+    variable->Bind(ConsumeExpression());
     if (!ParseStatement())
       return false;
     ProduceStatement(factory()->NewUsingStatement(
@@ -648,20 +647,20 @@ void Parser::ParseVariables(Token* keyword, ast::Type* type) {
   while (PeekToken()->is_name()) {
     auto const name = ConsumeToken();
     auto const assign = ConsumeTokenIf(TokenType::Assign);
-    auto const init =
-        assign && ParseExpression() ? ConsumeExpression() : nullptr;
-    if (!init) {
-      if (assign)
-        Error(ErrorCode::SyntaxVarAssign);
-      else if (keyword == TokenType::Const)
-        Error(ErrorCode::SyntaxVarConst);
-    }
-    auto const variable = factory()->NewVariable(keyword, type, name, init);
+    auto const variable = factory()->NewVariable(keyword, type, name);
     if (FindLocalMember(name))
       Error(ErrorCode::SyntaxVarDuplicate, name);
     else
       declaration_space_->AddMember(variable);
     variables.push_back(variable);
+    auto const init =
+        assign && ParseExpression() ? ConsumeExpression() : nullptr;
+    if (init)
+      variable->Bind(init);
+    else if (assign)
+      Error(ErrorCode::SyntaxVarAssign);
+    else if (keyword == TokenType::Const)
+      Error(ErrorCode::SyntaxVarConst);
     if (!AdvanceIf(TokenType::Comma))
       break;
     if (!PeekToken()->is_name())

elang/compiler/ast/expressions.cc
@@ -129,8 +129,8 @@ UnaryOperation::UnaryOperation(Token* op, Expression* expression)
 }
 
 // Variable
-Variable::Variable(Token* keyword, Type* type, Token* name, Expression* value)
-    : NamedNode(nullptr, keyword, name), type_(type), value_(value) {
+Variable::Variable(Token* keyword, Type* type, Token* name)
+    : NamedNode(nullptr, keyword, name), type_(type), value_(nullptr) {
   DCHECK(keyword == type->token() || keyword == TokenType::Const ||
          keyword == TokenType::Catch || keyword == TokenType::For ||
          keyword == TokenType::Using)
@@ -139,6 +139,12 @@ Variable::Variable(Token* keyword, Type* type, Token* name, Expression* value)
 
 bool Variable::is_const() const {
   return token() == TokenType::Const || token() == TokenType::Using;
+}
+
+void Variable::Bind(Expression* value) {
+  DCHECK(value);
+  DCHECK(!value_);
+  value_ = value;
 }
 
 // VariableReference

elang/compiler/ast/expressions.h
@@ -252,16 +252,19 @@ class Variable final : public NamedNode {
   DECLARE_CONCRETE_AST_NODE_CLASS(Variable, NamedNode);
 
  public:
+  bool is_bound() const { return value_ != nullptr; }
   bool is_const() const;
   Type* type() const { return type_; }
   Expression* value() const { return value_; }
 
+  void Bind(Expression* value);
+
  private:
-  // |keyword| one of 'catch', 'const', 'for', 'using', or |nullptr|
-  Variable(Token* keyword, Type* type, Token* name, Expression* value);
+  // |keyword| one of 'catch', 'const', 'for', 'using', or |type|.
+  Variable(Token* keyword, Type* type, Token* name);
 
   Type* const type_;
-  Expression* const value_;
+  Expression* value_;
 
   DISALLOW_COPY_AND_ASSIGN(Variable);
 };

elang/compiler/ast/factory.cc
@@ -356,11 +356,8 @@ Parameter* Factory::NewParameter(Method* owner,
   return new (zone_) Parameter(owner, kind, position, type, name, value);
 }
 
-Variable* Factory::NewVariable(Token* keyword,
-                               Type* type,
-                               Token* name,
-                               Expression* value) {
-  return new (zone_) Variable(keyword, type, name, value);
+Variable* Factory::NewVariable(Token* keyword, Type* type, Token* name) {
+  return new (zone_) Variable(keyword, type, name);
 }
 
 //////////////////////////////////////////////////////////////////////

elang/compiler/ast/factory.h
@@ -158,10 +158,7 @@ class Factory final {
                           Type* type,
                           Token* name,
                           Expression* expression);
-  Variable* NewVariable(Token* keyword,
-                        Type* type,
-                        Token* name,
-                        Expression* expression);
+  Variable* NewVariable(Token* keyword, Type* type, Token* name);
 
  private:
   Node* RememberNode(Node* node);

elang/compiler/public/compiler_error_code.h
@@ -110,6 +110,7 @@ namespace compiler {
   E(Syntax, Expression, RightParenthesis)             \
   E(Syntax, Expression, RightSquareBracket)           \
   E(Syntax, Expression, Type)                         \
+  E(Syntax, Expression, UnboundVariable)              \
   /* Finally */                                       \
   E(Syntax, Finally, LeftCurryBracket)                \
   /* For */                                           \

elang/compiler/syntax/parse_expression.cc
@@ -198,6 +198,8 @@ bool Parser::ParsePrimaryExpression() {
     if (auto const local_member = FindLocalMember(name)) {
       // Local name reference
       if (auto const var = local_member->as<ast::Variable>()) {
+        if (!var->is_bound())
+          Error(ErrorCode::SyntaxExpressionUnboundVariable, name);
         ProduceVariableReference(name, var);
       } else if (auto const param = local_member->as<ast::Parameter>()) {
         ProduceVariableReference(name, param);

elang/compiler/syntax/parser_test.cc
@@ -1050,6 +1050,14 @@ TEST_F(ParserTest, VarErrorDuplicate) {
   EXPECT_EQ("Syntax.Var.Duplicate(34) x\n", Format(source_code));
 }
 
+TEST_F(ParserTest, VarErrorForwardReference) {
+  Prepare(
+      "class A {"
+      "  static int Foo() { var x = x + 1; return x; }"
+      "}");
+  EXPECT_EQ("Syntax.Expression.UnboundVariable(38) x\n", Format());
+}
+
 TEST_F(ParserTest, VarErrorName) {
   auto const source_code =
       "class A {\n"

commit c63da4890eb7cd6d78c103bfa705cb08eaf0c14d

elang/compiler/syntax: Make parser to parse for-each statement correctly. This patch fixes broken for-each parser introduced by commit e147b2a514965260a6b84b1352d27b855527d33b.

elang/compiler/syntax/parse_statement.cc
@@ -372,17 +372,25 @@ bool Parser::ParseForStatement(Token* for_keyword) {
         auto const variable = factory()->NewVariable(for_keyword, type, name);
         declaration_space_->AddMember(variable);
         variables.push_back(variable);
-        auto const init = AdvanceIf(TokenType::Assign) && ParseExpression()
-                              ? ConsumeExpression()
-                              : static_cast<ast::Expression*>(nullptr);
-        variable->Bind(init);
-
         if (PeekToken() == TokenType::Colon) {
-          if (init)
-            Error(ErrorCode::SyntaxForColon);
+          // Bind 'for-each' variable to dummy expression to avoid unbound
+          // variable error.
+          variable->Bind(factory()->NewVariableReference(name, variable));
           state = State::Colon;
           continue;
         }
+
+        if (!AdvanceIf(TokenType::Assign)) {
+          Error(ErrorCode::SyntaxForInit);
+          continue;
+        }
+
+        if (!ParseExpression()) {
+          ProduceType(type);
+          continue;
+        }
+
+        variable->Bind(ConsumeExpression());
 
         if (PeekToken() == TokenType::SemiColon) {
           state = State::SemiColon;