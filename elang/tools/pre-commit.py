5fc931d96f24bd76b2722be10de68dba8628cd1e
Eval <eval1749@gmail.com>
2015-05-02 19:25:00 +0900

    elang/tools: Don't apply CPPLINT to deleted files.

--- elang/tools/pre-commit.py
+++ elang/tools/pre-commit.py
@@ -7,16 +7,19 @@ import sys
 
 def main():
     git_pipe = pipes.Template()
-    git_pipe.prepend('git diff --cached --name-only', '.-')
-    file = git_pipe.open('files', 'r')
-    lines = file.readlines()
+    git_pipe.prepend('git diff --cached --name-status', '.-')
+    diff_output = git_pipe.open('files', 'r')
+    lines = diff_output.readlines()
     exit_code = 0
     for line in lines:
         line = line.rstrip();
         if len(line) == 0:
             continue
+        words = line.split()
+        if words[0] == 'D':
+            continue
         cpplint_pipe = pipes.Template()
-        command_line = 'cpplint %(name)s 2>&1' % {'name': line}
+        command_line = 'cpplint %(name)s 2>&1' % {'name': words[1]}
         cpplint_pipe.prepend(command_line, '.-');
         outputs = cpplint_pipe.open('files', 'r').readlines()
         if outputs[len(outputs) - 1] == 'Total errors found: 0\n':
@@ -25,7 +28,7 @@ def main():
         for output in outputs:
             output = output.rstrip()
             print output
-    file.close()
+    diff_output.close()
     sys.exit(exit_code)
 
 if __name__ == '__main__':
