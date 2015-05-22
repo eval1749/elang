c77880101cab1d16445ce8ec92cee2fc72e7e297
eval1749 <eval1749@gmail.com>
2015-05-22 15:42:52 +0900

    elang/optimizer/scheduler: Utilize "\l" syntax of Graphviz label value for |VisualScheudle|.

--- elang/optimizer/scheduler/visual_schedule.cc
+++ elang/optimizer/scheduler/visual_schedule.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <unordered_map>
+
 #include "elang/optimizer/scheduler/visual_schedule.h"
 
 #include "base/strings/stringprintf.h"
@@ -33,43 +35,46 @@ std::ostream& operator<<(std::ostream& ostream, const VisualSchedule& wrapper) {
           << "  node [fontname=monospace fontsize=10]" << std::endl
           << "  overlap=false" << std::endl
           << "  rankdir=\"TB\"" << std::endl
-          << "  ranksep=\"0.2 equally\"" << std::endl
+          << "  ranksep=\"0.8 equally\"" << std::endl
           << "  splines=true" << std::endl
+          << "  node [shape=box]" << std::endl
           << std::endl;
-
+  std::unordered_map<Node*, Node*> end_to_start_map;
+  Node* start_node = nullptr;
   auto& schedule = *wrapper.schedule;
-  auto last = static_cast<const Node*>(nullptr);
   for (auto const node : schedule.nodes()) {
     if (node->IsBlockStart()) {
-      ostream << "  subgraph cluster_" << node->id() << " {" << std::endl;
-      last = nullptr;
+      ostream << " node" << node->id() << " [label=\"";
+      start_node = node;
+    } else {
+      DCHECK(start_node);
     }
-    ostream << "    node" << node->id() << " [shape=record "
-            << "label=\"{{" << PrefixOf(node) << node->id() << "="
-            << node->mnemonic();
+    ostream << PrefixOf(node) << node->id() << "=" << node->mnemonic();
     for (auto const input : node->inputs())
-      ostream << "|" << PrefixOf(input) << input->id();
-    ostream << "}}\"]" << std::endl;
-    if (last) {
-      ostream << "    node" << last->id() << " -> node" << node->id()
-              << std::endl;
-    }
-    last = node;
-    if (node->IsBlockEnd())
-      ostream << "  }" << std::endl;
+      ostream << " " << PrefixOf(input) << input->id();
+    ostream << "\\l";
+    if (!node->IsBlockEnd())
+      continue;
+    ostream << "\"]" << std::endl;
+    end_to_start_map.insert(std::make_pair(node, start_node));
+    start_node = nullptr;
   }
 
-  ostream << std::endl
-          << "  // Edges" << std::endl;
+  ostream << std::endl << "  // Edges" << std::endl;
   for (auto const node : schedule.nodes()) {
     if (!node->IsBlockStart())
       continue;
     auto position = 0;
-    auto const to = node->id();
+    auto const to = node;
     for (auto const input : node->inputs()) {
-      auto const from = input->id();
-      ostream << "  node" << from << " -> node" << to;
-      if (position && node->opcode() == Opcode::Loop)
+      auto const it = end_to_start_map.find(input);
+      DCHECK(it != end_to_start_map.end());
+      auto const from = it->second;
+      auto const is_back_edge = position && node->opcode() == Opcode::Loop;
+      auto const port = is_back_edge ? ":w" : "";
+      ostream << "  node" << from->id() << port << " -> node" << to->id()
+              << port;
+      if (is_back_edge)
         ostream << " [color=red constraint=false]";
       ostream << std::endl;
       ++position;
