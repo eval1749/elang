950f2b84bfe597eb889d099ae0fd4e9219589f34
eval1749 <eval1749@gmail.com>
2015-06-01 13:47:23 +0900

    elang/compiler: Use |sm::Method| instead of |ast::Method| to hold compiled funcitons in |CompilationSession|.

--- elang/compiler/translate/translate.cc
+++ elang/compiler/translate/translate.cc
@@ -2,12 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <utility>
+
 #include "elang/compiler/compilation_session.h"
 
+#include "elang/compiler/analysis/analysis.h"
 #include "elang/compiler/analysis/class_analyzer.h"
 #include "elang/compiler/analysis/method_analyzer.h"
 #include "elang/compiler/analysis/namespace_analyzer.h"
 #include "elang/compiler/analysis/name_resolver.h"
+#include "elang/compiler/ast/method.h"
+#include "elang/compiler/semantics/nodes.h"
 #include "elang/compiler/translate/translator.h"
 #include "elang/optimizer/editor.h"
 #include "elang/optimizer/factory.h"
@@ -38,15 +43,19 @@ void CompilationSession::Compile(NameResolver* name_resolver,
   Translator(this, factory).Run();
 }
 
-ir::Function* CompilationSession::IrFunctionOf(ast::Method* method) {
+ir::Function* CompilationSession::IrFunctionOf(ast::Method* ast_method) {
+  auto const method = analysis()->SemanticOf(ast_method)->as<sm::Method>();
+  DCHECK(method) << ast_method;
   auto const it = ir_function_map_.find(method);
   return it == ir_function_map_.end() ? nullptr : it->second;
 }
 
-void CompilationSession::RegisterFunction(ast::Method* method,
+void CompilationSession::RegisterFunction(ast::Method* ast_method,
                                           ir::Function* function) {
+  auto const method = analysis()->SemanticOf(ast_method)->as<sm::Method>();
+  DCHECK(method) << ast_method;
   DCHECK(!ir_function_map_.count(method));
-  ir_function_map_[method] = function;
+  ir_function_map_.insert(std::make_pair(method, function));
 }
 
 }  // namespace compiler
